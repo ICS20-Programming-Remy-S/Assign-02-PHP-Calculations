15abc90f94cc313399c7978ddb5656a04f375ade
Remy Skelton <124379820+Messi-Sui@users.noreply.github.com>
2023-03-31 02:13:32 +0000

    Completed

---
+++ results.php
@@ -0,0 +1,13 @@
+<?php
+  
+	// get the three messurements from the textfields
+	$aEdge = $_POST['aEdge'];
+
+	// calculate the pay
+  $surfaceArea = sqrt(3) * pow($aEdge, 2);
+  $volume = pow($aEdge,3) / (6 * sqrt(2));
+?>
+<?php echo "<h3>Results:</h3>
+  The surface area of your tetrahedron is " . round($surfaceArea, 2) . "cm<sup>2</sup>" .
+  "<br>The volume of your tetrahedron is " . round($volume, 2) . "cm<sup>3</sup>."
+?>
